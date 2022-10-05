6fbc0014e611de9b47cb79efa5b370c968307672
GabrielPenaHdz <Pena.hernandez.gabriel15@gmail.com>
2022-10-05 13:33:58 -0500

    OLa

--- app/src/main/java/com/example/myjson/MainActivity.java
+++ app/src/main/java/com/example/myjson/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Toast;
 import com.example.myjson.json.Myinfo;
 import com.google.gson.Gson;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
@@ -17,8 +19,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Myinfo myinfo = null;
+        Myinfo myinfo2 = null;
         Gson gson = null;
         String json = null;
+        String json2 = null;
+        List<Myinfo> list =null;
         String mensaje = null;
 
         myinfo = new Myinfo();
@@ -36,6 +41,16 @@ public class MainActivity extends AppCompatActivity {
         else{
             mensaje = "Error";
         }
-        Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_LONG);
+        //Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_LONG);}
+        json2 = "{\"calificacion\":9.5,\"edad\":17,\"nombre\":\"Gerdoc 1\",\"sexo\":true}";
+        myinfo2 = gson.fromJson(json2, Myinfo.class);
+        if(myinfo2 != null){
+            mensaje = String.format( "(%s)(%d)(%f)(%s)" , myinfo2.getNombre() , myinfo2.getEdad() , myinfo2.getCalificacion(), myinfo2.getSexo() ? "F": "M");
+        }
+        else
+        {
+            mensaje = "Error2";
+        }
+        Log.d(TAG, mensaje);
     }
 }
--- app/src/main/java/com/example/myjson/json/Myinfo.java
+++ app/src/main/java/com/example/myjson/json/Myinfo.java
@@ -3,40 +3,52 @@ package com.example.myjson.json;
 import java.io.Serializable;
 
 public class Myinfo implements Serializable {
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Integer getEdad() {
-        return edad;
-    }
-
-    public void setEdad(Integer edad) {
-        this.edad = edad;
-    }
-
-    public Float getCalificacion() {
-        return calificacion;
-    }
-
-    public void setCalificacion(Float calificacion) {
-        this.calificacion = calificacion;
-    }
-
-    public boolean isSexo() {
-        return sexo;
-    }
-
-    public void setSexo(boolean sexo) {
-        this.sexo = sexo;
-    }
-
     private String nombre;
     private Integer edad;
     private Float calificacion;
-    private boolean sexo;
+    private Boolean sexo;
+
+    public Myinfo()
+    {
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad()
+    {
+        return edad;
+    }
+
+    public void setEdad(Integer edad)
+    {
+        this.edad = edad;
+    }
+
+    public Float getCalificacion()
+    {
+        return calificacion;
+    }
+
+    public void setCalificacion(Float calificacion)
+    {
+        this.calificacion = calificacion;
+    }
+
+    public Boolean getSexo()
+    {
+        return sexo;
+    }
+
+    public void setSexo(Boolean sexo)
+    {
+        this.sexo = sexo;
+    }
 }
